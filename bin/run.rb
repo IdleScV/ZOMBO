0fd1048ae1a7137b3a1a9f17b6dda24fd1852e2d
IdleScV <weschen1996@gmail.com>
2020-02-10 17:58:24 -0500

    Add some intro

--- bin/run.rb
+++ bin/run.rb
@@ -1,16 +1,16 @@
 require_relative '../config/environment'
 
 
-
-puts "HELLO CHILD, PLEASE TYPE IN YOUR NAME"
+space =  " "
+"HELLO CHILD, PLEASE TYPE IN YOUR NAME...".each_char {|c| putc c ; sleep 0.05; $stdout.flush }
+puts space
 player_name = gets.chomp
+"RECEIVED!".each_char {|c| putc c ; sleep 0.15; $stdout.flush }
+puts space
 
 
 
-
-puts <<-'EOF'
-*********************************************
-
+title = <<-'EOF'
 
 _______  _______  _______  ______   _______ 
 / ___   )(  ___  )(       )(  ___ \ (  ___  )
@@ -25,6 +25,18 @@ _______  _______  _______  ______   _______
 
 EOF
 
-puts "OK, #{player_name}. LETS PLAY!"
-puts "*********************************************"
+bar = "*********************************************"
 
+bar.each_char {|c| putc c ; sleep 0.02; $stdout.flush }
+puts space
+title.each_char {|c| putc c ; sleep 0.005; $stdout.flush }
+puts space
+bar.each_char {|c| putc c ; sleep 0.02; $stdout.flush }
+puts space
+"OK, #{player_name}. LETS PLAY!".each_char {|c| putc c ; sleep 0.10; $stdout.flush }
+puts space
+"The rules to this game is simple...".each_char {|c| putc c ; sleep 0.10; $stdout.flush }
+puts space
+"1. Your gang of 5 must survive the zombie attack"
+puts space
+"2. Each round,"
