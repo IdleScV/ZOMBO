6b2cc99da8d801e7c83705d289e419876906da97
github username <laurenkyu@gmail.com>
2020-02-10 18:00:19 -0500

    choose character

---
+++ bin/cli_applications.rb
@@ -0,0 +1,26 @@
+require_relative '../config/environment'
+require 'pry'
+
+def choose_character
+    puts "Select your character..."
+    character_names = Character.all.map do |character_obj|
+        character_obj[:name] 
+    end
+
+    count = 0
+    while count < character_names.length
+        puts "#{count + 1}. #{character_names[count]}"
+        count += 1
+    end
+
+    puts "Enter the number of your champion."
+ 
+    input = gets.chomp.to_i
+
+    new_count = input - 1
+    puts "You've selected #{character_names[new_count]}"
+    return character_names[new_count]
+
+end
+
+choose_character
