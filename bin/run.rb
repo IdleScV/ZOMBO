a333b1c2be212338043fa4816c7e45aa25ca5cd2
IdleScV <weschen1996@gmail.com>
2020-02-10 20:46:10 -0500

    Update run.rb

--- bin/run.rb
+++ bin/run.rb
@@ -1,14 +1,13 @@
 require_relative '../config/environment'
 
-
-space =  " "
-"HELLO CHILD, PLEASE TYPE IN YOUR NAME...".each_char {|c| putc c ; sleep 0.05; $stdout.flush }
-puts space
-player_name = gets.chomp
-"RECEIVED!".each_char {|c| putc c ; sleep 0.15; $stdout.flush }
-puts space
+def scroll(text, mili_s=0.04)
+    text.each_char{|c| putc c ; sleep mili_s; $stdout.flush }
+end
 
 
+def line
+    puts  " "
+end
 
 title = <<-'EOF'
 
@@ -27,16 +26,30 @@ EOF
 
 bar = "*********************************************"
 
-bar.each_char {|c| putc c ; sleep 0.02; $stdout.flush }
-puts space
-title.each_char {|c| putc c ; sleep 0.005; $stdout.flush }
-puts space
-bar.each_char {|c| putc c ; sleep 0.02; $stdout.flush }
-puts space
-"OK, #{player_name}. LETS PLAY!".each_char {|c| putc c ; sleep 0.10; $stdout.flush }
-puts space
-"The rules to this game is simple...".each_char {|c| putc c ; sleep 0.10; $stdout.flush }
-puts space
-"1. Your gang of 5 must survive the zombie attack"
-puts space
-"2. Each round,"
+
+
+scroll("Hello player, please type in your name . . . ")
+line
+player_name = gets.chomp
+scroll(". . . . . Initiate!", 0.15)
+line
+scroll(bar, 0.02)
+line
+scroll(title, 0.007)
+line
+scroll(bar, 0.02)
+line
+scroll("#{player_name}, let's play!")
+line
+scroll("The rules to this game is simple...")
+line
+scroll("1. Your gang of 5 must survive the zombie attack")
+line
+scroll("2. Each round, pick a member and a weapon to face off against the zombie wave")
+line
+scroll("3. If you kill all 10 zombies, you win.")
+line
+scroll("3. If a party member loses to a zombie, they become a zombie.")
+line
+scroll("4. There are 10 zombies.")
+line
