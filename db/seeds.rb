afbc044f1d76d46c8dca80f19c561c35d010a483
github username <laurenkyu@gmail.com>
2020-02-12 16:53:00 -0500

    updated seed names

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ Zombie.destroy_all
 Fight.destroy_all
 
 5.times {
-Character.create(name: Faker::Name.name, catchphrase: Faker::TvShows::MichaelScott.quote, health: 10, alive: 1)
+Character.create(name: Faker::TvShows::Friends.character, catchphrase: Faker::TvShows::MichaelScott.quote, health: 10, alive: 1)
 }
 
 
@@ -21,7 +21,7 @@ Weapon.create(weapon_name: "rope", strength: 1)
 Weapon.create(weapon_name: "candlestick", strength: 10)
 
 10.times {
-Zombie.create(name: Faker::Games::Witcher.monster, catchphrase: Faker::TvShows::Community.quotes, health: rand(4..9), alive: 1)
+Zombie.create(name: Faker::Games::Witcher.monster, catchphrase: Faker::TvShows::DumbAndDumber.quote, health: rand(4..9), alive: 1)
 }
 
 scroll( "A new game has been rendered")
