b5648767a0760f8c15efbb06e51430291e429481
IdleScV <weschen1996@gmail.com>
2020-02-12 17:03:50 -0500

    updates small

--- bin/components/games/rope_game.rb
+++ bin/components/games/rope_game.rb
@@ -22,15 +22,15 @@ def rope_game_mini
     }
 
     #? Our Introduction to how the game is played
-    breaker = "====================================================================="
+    breaker = "============================================"
     intro = ["You have chosen to fight #{zombie["name"]} with a rope. . . .",
     "Excellent choice?",
     "Guess the correct animal to kill the zombie",
     "Your animal is #{word.length} letters long."]
-    step1 = "Please choose a letter from A to Z"
+    step1 = "Please choose a letter from 'a' to 'z'"
 
     #? Method that allows the game to be played.
-    def changeword( wordarr, blankarr)
+    def changeword(wordarr, blankarr)
         char = gets.chomp
         input = char.downcase
         indexes = wordarr.each_index.select{|i| wordarr[i] == input}
@@ -41,13 +41,19 @@ def rope_game_mini
                     scroll(input + " is in the word!")
                     blankarr[indexes[count]] = input
                     count+=1
+                    scroll("============================================", 0.005)
+                    p blankarr
                 end
+                return true
             #? action if letter guessed is false
             else 
                 scroll("'#{input}' is not a letter in the word")
+                scroll("============================================", 0.005)
+                p blankarr
+                return false
             end
         #? Returns the blank array with correct
-        p blankarr
+        
     end
 
 
@@ -55,28 +61,40 @@ def rope_game_mini
         #? Game Intro
         puts breaker
         intro.map{|x| scroll(x)}
+        p blankarr
         puts breaker
         #? Chances is 6
-        chances = 10
+        chances = 6
         count = 0
+        
         while count < chances do
             scroll(step1)
             scroll( "You have #{chances - count} chances left")
             #? wrong guess will increase count by 1
-            oldblankarr = blankarr
-            changeword(wordarr, blankarr)
-            if (oldblankarr.join('') == blankarr.join(''))
+
+            # p word #! Testing
+            change = changeword(wordarr, blankarr)
+            if change == false
                 count += 1
             end
             # ! Win 
             if blankarr.join('') == wordarr.join('')
-                scroll( "You did it!")
+                puts (' ')
+                puts (' ')
+                scroll("                            You did it!             ")
+                puts (' ')
+                scroll("You have wrangled #{zombie.name} to death with your excellent roping skills!")
                 return true
             end        
         end
     # ! Lost
-    scroll( "You have lost! the animal was #{wordarr.join('')}. . . .")
+    puts (' ')
+    puts (' ')
+    scroll("You have lost! the animal was #{wordarr.join('')}. . . .")
+    puts (' ')
+    scroll("You have been wrangled #{zombie.name} to death by your own rope.")
+    
     return false
 end
 
-# puts rope_game_mini
+puts rope_game_mini
